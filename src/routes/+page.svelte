2bc8430ca34f16573aed194532440c2de8cf4f0d
Chinmay Shet <chishxd@gmail.com>
2025-10-21 16:05:00 +0530

    Add basic UI concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { timeline } from '$lib/data';
+</script>
+
+<main>
+	{#each timeline as event (event.id)}
+		<section class="flex min-h-screen flex-col items-center justify-center border-b-2 p-8">
+			<div class="text-center">
+				<div class="mb-4 text-4xl">🌧️</div>
+				<h2 class="mb-2 text-4xl font-bold">{event.season}</h2>
+				<p class="max-w-md text-lg text-gray-700">{event.description}</p>
+			</div>
+		</section>
+	{/each}
+</main>
