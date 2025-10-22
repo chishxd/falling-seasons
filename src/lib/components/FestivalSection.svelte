03a7ee9a449cf22ac684ff36dcc6cefaa4bf8024
Chinmay Shet <chishxd@gmail.com>
2025-10-22 14:05:35 +0530

    Refactor content of event into it's own file

---
+++ src/lib/components/FestivalSection.svelte
@@ -0,0 +1,16 @@
+<script>
+	/**
+	 * @type {import{'../data'}.Festival}
+	 */
+	export let event;
+</script>
+
+<section
+	class="flex min-h-dvh w-full flex-col items-center justify-center border-b-2 border-white/10 p-8 font-inter"
+>
+	<div class="mx-auto max-w-2xl text-center">
+		<div class="mb-4 text-4xl">🌧️</div>
+		<h2 class="mb-4 text-4xl font-bold md:text-6xl">{event.season}</h2>
+		<p class="text-lg md:text-xl">{event.description}</p>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script>
 	import { timeline } from '$lib/data';
+	import FestivalSection from '$lib/components/FestivalSection.svelte';
 </script>
 
-<main>
+<main class="bg-green-200">
 	{#each timeline as event (event.id)}
-		<section class="flex min-h-screen flex-col items-center justify-center border-b-2 p-8">
-			<div class="text-center">
-				<div class="mb-4 text-4xl">🌧️</div>
-				<h2 class="mb-2 text-4xl font-bold">{event.season}</h2>
-				<p class="max-w-md text-lg text-gray-700">{event.description}</p>
-			</div>
-		</section>
+		<FestivalSection {event} />
 	{/each}
 </main>
