311850944e0ed7badca78ca864761f44dcf60f12
Chinmay Shet <chishxd@gmail.com>
2025-10-22 15:25:18 +0530

    feat: Add animation to each element's arrival on screen

--- src/lib/components/FestivalSection.svelte
+++ src/lib/components/FestivalSection.svelte
@@ -1,4 +1,30 @@
 <script>
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	let sectionElement; //Binds with scroll animations
+
+	onMount(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const anim = gsap.from(sectionElement, {
+			opacity: 0,
+			y: 50,
+			duration: 1,
+			ease: 'power3.out',
+			scrollTrigger: {
+				trigger: sectionElement,
+				start: 'top 80%',
+				// markers: true, // Enable for Debugging process
+
+				toggleActions: 'play none reverse noner'
+			}
+		});
+
+		return () => {
+			anim.kill();
+		};
+	});
+
 	/**
 	 * @type {import{'../data'}.Festival}
 	 */
@@ -6,6 +32,7 @@
 </script>
 
 <section
+	bind:this={sectionElement}
 	class="flex min-h-dvh w-full flex-col items-center justify-center border-b-2 border-white/10 p-8 font-inter"
 >
 	<div class="mx-auto max-w-2xl text-center">
