546399662c8154c9f45e9d338cf92da067957d6a
Chinmay Shet <chishxd@gmail.com>
2025-10-24 15:12:51 +0530

    Add basic functions for index

---
+++ src/lib/components/StoryModal.svelte
@@ -0,0 +1,19 @@
+<script>
+	let currentSlideIndex = 0;
+
+	function prevSlide() {
+		if (currentSlideIndex !== 0) {
+			currentSlideIndex--;
+		}
+		return;
+	}
+
+	function nextSlide() {
+		if (currentSlideIndex < festival.story.length - 1) {
+			currentSlideIndex++;
+		}
+		return;
+	}
+
+	export let festival;
+</script>
